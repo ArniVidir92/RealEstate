f3a52e4f271c6e408bea1ff7cf799e48760ceb4b
ArniVidir92 <avj2@hi.is>
2016-09-26 17:19:07 +0000

    Bjó til file fyrir setUp
    Það ætti samt bara að vera keyrt ef ekki er nú þegar búið að keyra hann.

---
+++ setUp.R
@@ -0,0 +1,44 @@
+# Þessi skrá sér um að fjarlægja þá dálka sem við þurfum ekki í módelinu
+
+library(ggplot2)
+library(plyr)
+library(faraway)
+library(car)
+library(gridExtra) 
+library(MASS)
+library(data.table)
+library(GGally)
+
+verdmat <-read.table("gagnasafn_endurmat2017_litid.csv", sep=',', header=T)
+RE_data <- data.frame(verdmat)
+dt.re <- data.table(verdmat)
+#Matsv:
+#  Vesturb. 11
+#  miðbær s þ 31
+#  hlíðar 80
+#  grafarvogur 120
+#  seljahverfi 150
+
+dt.verdmat <- data.table(verdmat)
+dt.RE_data <- dt.verdmat[matssvaedi %in% c(11,31,80,120,150)]
+# Bæta við dálki kaupár
+dt.RE_data <- dt.RE_data[,k.ar:=substring(kdagur,1,4)]
+
+# Byrjum á að taka út þær breytur sem við teljum ekki  
+dt.RE_data <- subset(dt.RE_data, select=c("kdagur","nuvirdi","teg_eign","byggar","haednr","lyfta","ibm2","fjhaed",
+                                          "fjbilast","fjbkar","fjsturt","fjklos","fjeld","fjherb",
+                                          "fjstof","fjgeym","stig10","ibteg","k.ar"))
+
+dt.RE_data <- dt.RE_data[,kdagur:=as.Date(kdagur)]
+
+#breyta lyftu í boolean og breyta ibteg í factor, 11=serbýli, 12=fjölbýli
+
+#for(i in 1:n){
+#  if(dt.RE_data$lyfta[i]!=0){dt.RE_data$lyfta[i]=1}
+#  else{}
+#}
+
+for(i in 1:nrow(dt.RE_data)){
+  if(dt.RE_data$ibteg[i]==11){dt.RE_data$ibteg="Sérbýli"}
+  else{dt.RE_data$ibteg[i]="Fjölbýli"}
+}
