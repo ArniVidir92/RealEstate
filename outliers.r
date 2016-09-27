59a528bfbd38446983316492e1ac73d59ea30d68
ottohr <ohr2@hi.is>
2016-09-27 21:39:39 +0000

    einföldun

--- outliers.r
+++ outliers.r
@@ -1,26 +1,5 @@
 if(!exists("dt.RE_data")){ source("setUp.R")}
 
-diag <- data.table(fortify(lm.all))
-res_sd <- sd(diag$.resid)
-res_index <- ggplot(diag, aes(x=seq(1:length(.resid)),y=.resid)) + geom_point()
-res_index <- res_index + geom_hline(yintercept=0, col="red", linetype="dashed")
-res_index <- res_index + geom_hline(yintercept=2*res_sd, col="blue", linetype="dashed")
-res_index <- res_index + geom_hline(yintercept=-2*res_sd, col="blue", linetype="dashed")
-res_index <- res_index + geom_hline(yintercept=3*res_sd, col="green", linetype="dashed")
-res_index <- res_index + geom_hline(yintercept=-3*res_sd, col="green", linetype="dashed")
-res_index <- res_index + xlab("Index")+ylab("Residuals")+labs(title = "Index plot of residuals")
-res_index <- res_index + geom_text(aes(label=ifelse(abs(.resid)>2*res_sd,nuvirdi,"")),hjust=-.5, vjust=0.4)
-
-res_index
-
-sort(diag$nuvirdi)
-
-Jack_index<-ggplot(diag, aes(x=seq(1:length(.jack)),y=.jack))+geom_point()
-Jack_index<-Jack_index+geom_hline(yintercept=0, col="red", linetype="dashed")
-Jack_index<-Jack_index+xlab("Index")+ylab("Jackknife residuals")
-Jack_index<-Jack_index+geom_text(aes(label=ifelse(abs(.jack)>2.4,.index,"")),hjust=0, vjust=0)
-Jack_index
-
 indexPlotLeverage <- function(model){
   diag <- data.table(fortify(model))
   diag$.index = c(1:length(diag$.resid))
@@ -116,7 +95,7 @@ removeOutliersWStdResMoreThanThree <- function(model){
   k.ar <- rowsWithNoOutliers$k.ar
   return(
     lm(nuvirdi ~ kdagur + teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjbilast + fjbkar + fjsturt + 
-         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar)
+         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar, data=rowsWithNoOutliers)
     )
 }
 
@@ -144,6 +123,6 @@ removeInfluential <- function(model, maxCookDistance){
   k.ar <- rowsWithNoInfluentials$k.ar
   return(
     lm(nuvirdi ~ kdagur + teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjbilast + fjbkar + fjsturt + 
-         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar)
+         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar, data=rowsWithNoInfluentials)
   )
 }
--- setUp.R
+++ setUp.R
@@ -22,18 +22,23 @@ dt.re <- data.table(verdmat)
 dt.verdmat <- data.table(verdmat)
 dt.RE_data <- dt.verdmat[matssvaedi %in% c(11,31,80,120,150)]
 # Bæta við dálki kaupár
-dt.RE_data <- dt.RE_data[,k.ar:=substring(kdagur,1,4)]
+dt.RE_data <- dt.RE_data[,k.ar:=as.numeric(substring(kdagur,1,4))]
+dt.RE_data <- dt.RE_data[,matssvaedi:=as.factor(matssvaedi)]
+dt.RE_data <- dt.RE_data[,undirmatssvaedi:=as.factor(undirmatssvaedi)]
+#dt.RE_data <- dt.RE_data[,ibteg:=as.factor(ibteg)]
+dt.RE_data <- dt.RE_data[,teg_eign:=as.factor(teg_eign)]
+
 
 # Byrjum á að taka út þær breytur sem við teljum ekki  
 dt.RE_data <- subset(dt.RE_data, select=c("kdagur","nuvirdi","teg_eign","byggar","haednr","lyfta","ibm2","fjhaed",
                                           "fjbilast","fjbkar","fjsturt","fjklos","fjeld","fjherb",
-                                          "fjstof","fjgeym","stig10","ibteg","k.ar"))
+                                          "fjstof","fjgeym","stig10","matssvaedi","undirmatssvaedi","ibteg","k.ar"))
 
 dt.RE_data <- dt.RE_data[,kdagur:=as.Date(kdagur)]
 
 #breyta lyftu í boolean og breyta ibteg í factor, 11=serbýli, 12=fjölbýli
 
-for(i in 1:n){
+for(i in 1:nrow(dt.RE_data)){
   if(dt.RE_data$lyfta[i]!=0){dt.RE_data$lyfta[i]=1}
   else{}
 }
@@ -42,3 +47,17 @@ for(i in 1:nrow(dt.RE_data)){
   if(dt.RE_data$ibteg[i]==11){dt.RE_data$ibteg="Sérbýli"}
   else{dt.RE_data$ibteg[i]="Fjölbýli"}
 }
+
+
+dt.RE_data <- dt.RE_data[,ibteg:=as.factor(ibteg)]
+
+
+n<-dim(dt.RE_data)[1]
+
+# Training and testing data sets
+set.seed(5)
+rows<-sample(1:n,n/3)
+train <- data.table(dt.RE_data[-rows,])
+test <- data.table(dt.RE_data[rows,])
+
+lm.all1 <- lm(nuvirdi ~ ., data=train)
