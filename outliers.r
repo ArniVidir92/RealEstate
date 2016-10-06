22851b3a53c5418fa32fd9d2a03155eb03c84a4d
ottohr <ohr2@hi.is>
2016-10-06 15:12:01 +0000

    Breytingar í outliers ig diagnostics
    af

--- outliers.r
+++ outliers.r
@@ -18,7 +18,7 @@ fittedVsresiduals <- function(model){
   p<-length(coef(model))
   n<-length(fitted(model))
   fitVsres <-ggplot(diag, aes(x=.fitted,y=.resid))+geom_point()+ylim(-1.5*max(diag$.resid),1.5*max(diag$.resid))
-  fitVsres          
+  fitVsres <- fitVsres + labs(title="Fitted value against residuals", ylab="Fitted value",xlab="Residuals")          
 }
 
 
@@ -84,37 +84,37 @@ removeOutliersAndReturnNewModel <- function(model,alpha){
 
 
 # Þurfum líklega að laga þetta fall
-removeOutliersWStdResMoreThanThree <- function(model){
-  diag <- data.table(fortify(model))
-  rowsWithNoOutliers <- diag[abs(diag$.stdresid) < 3,]
-  kdagur <- rowsWithNoOutliers$kdagur         
-  nuvirdi <- rowsWithNoOutliers$nuvirdi        
-  teg_eign <- as.factor(rowsWithNoOutliers$teg_eign)      
-  byggar <- rowsWithNoOutliers$byggar         
-  haednr <- rowsWithNoOutliers$haednr        
-  lyfta <- rowsWithNoOutliers$lyfta       
-  ibm2  <- rowsWithNoOutliers$ibm2         
-  fjhaed <- rowsWithNoOutliers$fjhaed    
-  fjbilast  <- rowsWithNoOutliers$fjbilast    
-  fjbkar  <- rowsWithNoOutliers$fjbkar
-  fjsturt <- rowsWithNoOutliers$fjsturt
-  fjklos  <- rowsWithNoOutliers$fjklos 
-  fjeld  <- rowsWithNoOutliers$fjeld    
-  fjherb  <- rowsWithNoOutliers$fjherb
-  fjstof  <- rowsWithNoOutliers$fjstof    
-  fjgeym  <- rowsWithNoOutliers$fjgeym     
-  stig10  <- rowsWithNoOutliers$stig10   
-  ibteg <- as.factor(rowsWithNoOutliers$ibteg) 
-  k.ar <- rowsWithNoOutliers$k.ar
-  return(
-    lm(nuvirdi ~ kdagur + teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjbilast + fjbkar + fjsturt + 
-         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar, data=rowsWithNoOutliers)
-    )
-}
+#removeOutliersWStdResMoreThanThree <- function(model){
+#  diag <- data.table(fortify(model))
+#  rowsWithNoOutliers <- diag[abs(diag$.stdresid) < 3,]
+#  kdagur <- rowsWithNoOutliers$kdagur         
+#  nuvirdi <- rowsWithNoOutliers$nuvirdi        
+#  teg_eign <- as.factor(rowsWithNoOutliers$teg_eign)      
+#  byggar <- rowsWithNoOutliers$byggar         
+#  haednr <- rowsWithNoOutliers$haednr        
+#  lyfta <- rowsWithNoOutliers$lyfta       
+#  ibm2  <- rowsWithNoOutliers$ibm2         
+#  fjhaed <- rowsWithNoOutliers$fjhaed    
+#  fjbilast  <- rowsWithNoOutliers$fjbilast    
+#  fjbkar  <- rowsWithNoOutliers$fjbkar
+#  fjsturt <- rowsWithNoOutliers$fjsturt
+#  fjklos  <- rowsWithNoOutliers$fjklos 
+#  fjeld  <- rowsWithNoOutliers$fjeld    
+#  fjherb  <- rowsWithNoOutliers$fjherb
+#  fjstof  <- rowsWithNoOutliers$fjstof    
+#  fjgeym  <- rowsWithNoOutliers$fjgeym     
+#  stig10  <- rowsWithNoOutliers$stig10   
+#  ibteg <- as.factor(rowsWithNoOutliers$ibteg) 
+#  k.ar <- rowsWithNoOutliers$k.ar
+#  return(
+#    lm(nuvirdi ~ kdagur + teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjbilast + fjbkar + fjsturt + 
+#         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar, data=rowsWithNoOutliers)
+#    )
+#}
 
 #.hat,.sigma,.cooksd,.fitted.resid,.stdresid
 
-removeOutliersWStdResMoreThanThreePr <- function(model){
+removeOutliersWStdResMoreThanThree <- function(model){
   diag <- data.table(fortify(model))
   rowsWithNoOutliers <- diag[abs(diag$.stdresid) < 3,]
   rowsWithNoOutliers <- rowsWithNoOutliers[ ,c(".hat",".sigma",".cooksd",".fitted",".resid",".stdresid") := NULL]
@@ -152,28 +152,28 @@ removeOutliersWStdResMoreThanThreePr <- function(model){
 removeInfluential <- function(model, maxCookDistance){
   diag <- data.table(fortify(model))
   rowsWithNoInfluentials <- diag[diag$.cooksd < maxCookDistance,]
-  kdagur <- rowsWithNoInfluentials$kdagur         
-  nuvirdi <- rowsWithNoInfluentials$nuvirdi        
-  teg_eign <- as.factor(rowsWithNoInfluentials$teg_eign)      
-  byggar <- rowsWithNoInfluentials$byggar         
-  haednr <- rowsWithNoInfluentials$haednr        
-  lyfta <- rowsWithNoInfluentials$lyfta       
-  ibm2  <- rowsWithNoInfluentials$ibm2         
-  fjhaed <- rowsWithNoInfluentials$fjhaed    
-  fjbilast  <- rowsWithNoInfluentials$fjbilast    
-  fjbkar  <- rowsWithNoInfluentials$fjbkar
-  fjsturt <- rowsWithNoInfluentials$fjsturt
-  fjklos  <- rowsWithNoInfluentials$fjklos 
-  fjeld  <- rowsWithNoInfluentials$fjeld    
-  fjherb  <- rowsWithNoInfluentials$fjherb
-  fjstof  <- rowsWithNoInfluentials$fjstof    
-  fjgeym  <- rowsWithNoInfluentials$fjgeym     
-  stig10  <- rowsWithNoInfluentials$stig10   
-  ibteg <- as.factor(rowsWithNoInfluentials$ibteg) 
-  k.ar <- rowsWithNoInfluentials$k.ar
+  rowsWithNoInfluentials <- rowsWithNoInfluentials[ ,c(".hat",".sigma",".cooksd",".fitted",".resid",".stdresid") := NULL]
+  #kdagur <- rowsWithNoInfluentials$kdagur         
+  #nuvirdi <- rowsWithNoInfluentials$nuvirdi        
+  #teg_eign <- as.factor(rowsWithNoInfluentials$teg_eign)      
+  #byggar <- rowsWithNoInfluentials$byggar         
+  #haednr <- rowsWithNoInfluentials$haednr        
+  #lyfta <- rowsWithNoInfluentials$lyfta       
+  #ibm2  <- rowsWithNoInfluentials$ibm2         
+  #fjhaed <- rowsWithNoInfluentials$fjhaed    
+  #fjbilast  <- rowsWithNoInfluentials$fjbilast    
+  #fjbkar  <- rowsWithNoInfluentials$fjbkar
+  #fjsturt <- rowsWithNoInfluentials$fjsturt
+  #fjklos  <- rowsWithNoInfluentials$fjklos 
+  #fjeld  <- rowsWithNoInfluentials$fjeld    
+  #fjherb  <- rowsWithNoInfluentials$fjherb
+  #fjstof  <- rowsWithNoInfluentials$fjstof    
+  #fjgeym  <- rowsWithNoInfluentials$fjgeym     
+  #stig10  <- rowsWithNoInfluentials$stig10   
+  #ibteg <- as.factor(rowsWithNoInfluentials$ibteg) 
+  #k.ar <- rowsWithNoInfluentials$k.ar
   return(
-    lm(nuvirdi ~ kdagur + teg_eign + byggar + haednr + lyfta + ibm2 + fjhaed + fjbilast + fjbkar + fjsturt + 
-         fjklos + fjeld + fjherb + fjstof + fjgeym + stig10 + ibteg + k.ar, data=rowsWithNoInfluentials)
+    lm(nuvirdi ~ ., data=rowsWithNoInfluentials)
   )
 }
 
@@ -183,8 +183,8 @@ QQplotResiduals <- function(model){
   res_sd <- sd(diag$.resid)
   QQResP<-ggplot(diag, aes(sample = .resid)) + stat_qq()
   QQResP <- QQResP + geom_abline(slope=res_sd)
-  QQResP <- QQResP + ylab("Residuals")
-  QQResP
+ # QQResP <- QQResP + ylab("Residuals")
+  QQResP <- QQResP + labs(title="QQ plot", ylab="Theoretical",xlab="Residuals")
 }
 
 # Þetta fall virkar ekki
